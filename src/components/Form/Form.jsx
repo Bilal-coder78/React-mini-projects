b2c34f2f03ae7dbe1550db250986e9d21e676665
M Bilal <bilalsikander87@gmail.com>
2025-10-05 20:52:18 +0500

    form completed

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,36 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Form.css"
 import Person_icon from "../../assets/person.png"
 import email_icon from "../../assets/email.png"
 import password_icon from "../../assets/password.png"
 
 function Form() {
+    const [action,setAction] = useState("Sign Up");
     return (
         <div className='d-flex align-items-center justify-content-center flex-column contanier'>
-            <div class="Full-form rounded-3">
-                <div class="header d-flex flex-column gap-1 w-100 my-3 align-items-center">
-                    <h2 className='fs-1 fw-semibold'>Sign Up</h2>
-                    <div class="underline"></div>
+            <div className="Full-form rounded-3">
+                <div className="header d-flex flex-column gap-1 w-100 my-3 align-items-center">
+                    <h2 className='fs-1 fw-semibold'>{action}</h2>
+                    <div className="underline"></div>
                 </div>
                 <form action="" className='d-flex flex-column'>
-                    <div class="inputs mt-4 d-flex flex-column gap-4">
-                        <div class="input">
+                    <div className="inputs mt-4 d-flex flex-column gap-4">
+                        {action==="Login"?<div></div>:
+                        <div className="input">
                             <img src={Person_icon} alt="" />
                             <input type="text" name="" id="" placeholder='Name' />
-                        </div>
-                        <div class="input">
+                        </div>}
+                        <div className="input">
                             <img src={email_icon} alt="" />
                             <input type="email" name="" id="" placeholder='Email' />
                         </div>
-                        <div class="input">
+                        <div className="input">
                             <img src={password_icon} alt="" />
                             <input type="password" name="" id="" placeholder='Password' />
                         </div>
                     </div>
-                    <div class="forgot-password">forgot password? <span>Click Here!</span></div>
-                    <div class="buttons">
-                        <button>Sign Up</button>
-                        <button>Login</button>
+                    {action==="Sign Up"?<div></div>:
+                    <div className="forgot-password">forgot password? <span>Click Here!</span></div>}
+                    <div className="buttons">
+                        <button type='button' className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</button>
+                        <button type='button' className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</button>
                     </div>
                 </form>
             </div>
