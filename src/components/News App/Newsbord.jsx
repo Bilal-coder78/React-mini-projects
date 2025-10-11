f7c9e065a39fa16a8f9536d4ae68b0f58089189f
M Bilal <bilalsikander87@gmail.com>
2025-10-11 19:58:17 +0500

    fully responsive

--- src/components/News App/Newsbord.jsx
+++ src/components/News App/Newsbord.jsx
@@ -19,15 +19,21 @@ function Newsbord({ category }) {
     return (
         <>
             <div className='text-center fs-3'>Today <span className='bg-danger badge my-3'>News</span></div>
-            {articles.length === 0? <><h1 className='d-flex align-items-center justify-content-center mt-5'>Loading News...</h1></>:
-            <>
-            {articles.map((v, i) => {
-                return (
-                    <Newsitem key={i} title={v.title || "No title for that News"} description={v.description || "No description for that news"} src={v.urlToImage} url={v.url} />
-                )
-            })}
-            </>
-        }
+            {articles.length === 0 ? <><h1 className='d-flex align-items-center justify-content-center mt-5'>Loading News...</h1></> :
+                <>
+                    <div class="container-fluid">
+                        <div class="row justify-content-center">
+                            {articles.map((v, i) => {
+                                return (
+                                    <div class="col-12 d-flex justify-content-center align-items-center col-sm-auto">
+                                        <Newsitem key={i} title={v.title || "No title for that News"} description={v.description || "No description for that news"} src={v.urlToImage} url={v.url} />
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </>
+            }
         </>
     )
 }
